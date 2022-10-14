a4e0a603fbf72d7f786cc01c0dbeb2709faba92e
Michelle Byrnes <38360136+Michelleeby@users.noreply.github.com>
2022-10-14 11:53:56 -0400

    Create env.sh

---
+++ cli/env.sh
@@ -0,0 +1,8 @@
+# path to pokemon decomps root
+DECOMPS_ROOT_DIR=~/Games/decomps
+# path to pokemon decomps tools
+DECOMPS_TOOLS_DIR=$DECOMPS_ROOT_DIR/tools
+# path to pokemon decomps compiler
+DECOMPS_AGBCC=$DECOMPS_TOOLS_DIR/agbcc
+#path to pokemon decomps sources
+DECOMPS_SOURCE_DIR=$DECOMPS_ROOT_DIR/source
